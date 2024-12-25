07d150263a1c20d0a718a7d36430e547457ae7b0
GanExin <sam.ganexin@gmail.com>
2024-12-25 16:48:56 +0800

    homepage correction

--- admin/homepage.py
+++ admin/homepage.py
@@ -1,6 +1,6 @@
-from PythonAssignment.admin.vehicle_management_and_maintenance import manage_vehicle_and_maintenance
-from PythonAssignment.driver_management import manage_driver
-from PythonAssignment.admin.manage_fuel_and_consumption import fuel_management_and_vehicle_consumption
+from PythonAssignment.admin.driver_management import driver_management
+from PythonAssignment.admin.manage_fuel_and_consumption import manage_fuel_and_consumption
+from PythonAssignment.admin.vehicle_management_and_maintenance import vehicle_management_and_maintenance
 
 
 def admin_homepage(session):
@@ -10,11 +10,11 @@ def admin_homepage(session):
                 "[1] Manage Vehicle and Maintenance \n[2] Manage Driver \n[3] Fuel Management and Vehicle consumption \n[4] View Reports \n[5] Exit \nSelect the following options: "))
 
             if user_choice == 1:
-                manage_vehicle_and_maintenance(session)
+                vehicle_management_and_maintenance(session)
             elif user_choice == 2:
-                manage_driver(session)
+                driver_management(session)
             elif user_choice == 3:
-                fuel_management_and_vehicle_consumption(session)
+                manage_fuel_and_consumption(session)
             elif user_choice == 4:
                 pass #view_reports(session)
             elif user_choice == 5:
