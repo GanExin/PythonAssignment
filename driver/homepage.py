03b6eb3c49d43cee0e98c564ec07ae42559397ce
GanExin <sam.ganexin@gmail.com>
2025-01-04 23:50:08 +0800

    chore: delete unused imports

--- driver/homepage.py
+++ driver/homepage.py
@@ -1,6 +1,6 @@
 from PythonAssignment.admin.driver_management import view_comment_for_drivers
-from PythonAssignment.database import read_driver_details, read_users, update_parcel_details
-from PythonAssignment.driver.delivery_details import view_driver_orders, delivery_details
+from PythonAssignment.database import read_driver_details
+from PythonAssignment.driver.delivery_details import delivery_details
 from PythonAssignment.driver.parcel_and_order_details import update_parcel_driver
 from PythonAssignment.driver.manage_driver_profile import update_password, update_profile
 from PythonAssignment.driver.update_booked_parcel import update_booked_parcel_details
@@ -10,7 +10,6 @@ from PythonAssignment.driver.view_vehicle import view_assign_vehicle
 #homepage for drivers
 def driver_homepage(session):
     profile = read_driver_details(session[0])
-    users = read_users()
 
     print("Welcome back! " + profile[1] + "!")
 
